14b30b61c5c93105d00a7619b055997ab5092b04
Grandpabilly69 <mfairhurst05@gmail.com>
2025-10-04 11:35:24 +0200

    Comments added

--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,24 @@ mod syntax_analyzer;
 mod semantic_analyzer;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    //allows to use enums from lexer
     use lex_layer::Token::*;
     use lex_layer::LiteralType::*;
 
+    //creates tokens from lexer to use for syntax analyzer
     let mut buffer = std::string::String::new();
-
-    let contents = file_translate::read_file(&mut buffer)?;  // now `?` works
-
+    let contents = file_translate::read_file(&mut buffer)?;
     let tokens = lex_layer::tokenize::<std::io::Error>(Ok(contents))?;
 
 
+    //analyzes syntax of code and prepares tokens into parse tree
     let mut parser = syntax_analyzer::Parser::new(&tokens);
     match parser.parse_function() {
         Ok(func) => println!("{:#?}", func),
         Err(e) => eprintln!("Parse error: {}", e),
     }
 
+    //prepares tokens into parse tree for semantic analyzer
     let mut parser = syntax_analyzer::Parser::new(&tokens);
     match parser.parse_function() {
         Ok(func) => {
@@ -27,7 +29,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
             let mut sema = semantic_analyzer::SemanticAnalyzer::new();
             match sema.analyze_function(&func) {
-                Ok(_) => println!("Semantic analysis passed ✅"),
+                Ok(_) => println!("Semantic analysis passed"),
                 Err(e) => eprintln!("Semantic error: {}", e),
             }
         }
@@ -37,6 +39,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
+//this is for error checking by showing the tokens
 fn check_tokens() -> Result<(), std::io::Error> {
     let mut buffer = String::new();
     let contents = file_translate::read_file(&mut buffer)?;           // Result<&str, io::Error>
